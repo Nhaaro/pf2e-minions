f5a7cde585a3f2ea729158a2c7c68ec0a89497e1
Alan Laredo <AlanLaredo@hotmail.com>
2023-11-26 17:47:12 -0600

    feat(API): linkMinion module function

--- src/module.ts
+++ src/module.ts
@@ -7,7 +7,8 @@ import { registerHooks } from './module/index.ts';
 
 declare global {
     interface Module {
-        cleanUuids?(masterUuid?: string): void;
+        cleanUuids(masterUuid?: string): void;
+        linkMinion(type: string): void;
     }
     var pf2eMinions: Module;
 }
@@ -26,6 +27,30 @@ globalThis.pf2eMinions.cleanUuids = async masterUuid => {
     let actors = await Promise.all(uuids.map(async uuid => await fromUuid(uuid)));
     const minions = actors.filter(minion => minion).map(minion => minion?.uuid);
     master.setFlag(MODULE_NAME, 'minions', minions);
+};
+globalThis.pf2eMinions.linkMinion = async type => {
+    const master = canvas.tokens.controlled[0];
+    const minion = Array.from(game.user.targets)[0];
+
+    if (!master) {
+        console.error('no token selected');
+        return;
+    }
+    if (!minion) {
+        console.error('no token targeted');
+        return;
+    }
+
+    minion.document.setFlag('pf2e-minions', 'master', master.id);
+    minion.document.setFlag('pf2e-minions', 'type', type);
+    minion.actor?.setFlag('pf2e-minions', 'master', master.id);
+    minion.actor?.setFlag('pf2e-minions', 'type', type);
+    (minion.actor?.prototypeToken as any).setFlag?.('pf2e-minions', 'master', master.id);
+    (minion.actor?.prototypeToken as any).setFlag?.('pf2e-minions', 'type', type);
+
+    var minions = (master.actor?.getFlag('pf2e-minions', 'minions') as string[]) ?? [];
+    if (!minions.find(uuid => uuid === minion.document.uuid))
+        master.actor?.setFlag('pf2e-minions', 'minions', [...minions, minion.document.uuid]);
 };
 
 Hooks.once('init', async function () {
